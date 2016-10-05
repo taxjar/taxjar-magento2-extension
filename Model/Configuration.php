e7d93be43afedaa5213704dcc64c5022a738bc66
Jake Johnson <jake@fastdivision.com>
2016-10-04 17:24:02 -0700

    Version 0.6.0

--- Model/Configuration.php
+++ Model/Configuration.php
@@ -21,7 +21,7 @@ use Magento\Config\Model\ResourceModel\Config;
 
 class Configuration
 {
-    const TAXJAR_VERSION              = '0.5.2';
+    const TAXJAR_VERSION              = '0.6.0';
     const TAXJAR_APIKEY               = 'tax/taxjar/apikey';
     const TAXJAR_BACKUP               = 'tax/taxjar/backup';
     const TAXJAR_CATEGORIES           = 'tax/taxjar/categories';
