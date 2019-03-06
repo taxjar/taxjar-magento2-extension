aa94e6dacc24c9b23c1cebd927d4269a1d2fcc9b
Jake Johnson <jake@fastdivision.com>
2019-03-06 06:34:21 -0800

    Revert "Revert "Refactor front-end checkout to support shipping address form without saved addresses and guest checkout""
    This reverts commit 6bcb68cc24d30357144339b7228da0e36f27e9fb.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,12 +3,6 @@ var config = {
         mixins: {
             'Magento_Customer/js/addressValidation': {
                 'Taxjar_SalesTax/js/address_validation': true
-            },
-            'Magento_Checkout/js/view/shipping': {
-                'Taxjar_SalesTax/js/view/shipping': true
-            },
-            'Magento_Checkout/js/view/shipping-address/address-renderer/default': {
-                'Taxjar_SalesTax/js/view/shipping-address/address-renderer/default': true
             }
         }
     }
