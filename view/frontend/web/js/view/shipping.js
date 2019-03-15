55e17472d14e2b699ed0d8180612e8d169798795
Jake Johnson <jake@fastdivision.com>
2019-03-15 13:42:27 -0700

    Refresh suggested addresses on street / city / state field change in guest checkout form

--- view/frontend/web/js/view/shipping.js
+++ view/frontend/web/js/view/shipping.js
@@ -2,16 +2,34 @@ define([
         'ko',
         'jquery',
         'Magento_Checkout/js/model/quote',
-        'Magento_Customer/js/model/address-list'
+        'Magento_Customer/js/model/address-list',
+        'Taxjar_SalesTax/js/model/address_validation_core'
     ], function (
         ko,
         $,
         quote,
-        addressList
+        addressList,
+        avCore
     ) {
         'use strict';
 
         var mixin = {
+            initialize: function () {
+                this._super();
+
+                var self = this;
+
+                $(document).on('change', '[name="street[0]"], [name="city"], [name="region_id"]', function () {
+                    avCore.getSuggestedAddresses({
+                        street: [self.source.get('shippingAddress.street.0')],
+                        city: self.source.get('shippingAddress.city'),
+                        regionId: self.source.get('shippingAddress.region_id'),
+                        postcode: self.source.get('shippingAddress.postcode'),
+                        countryId: self.source.get('shippingAddress.country_id')
+                    });
+                });
+            },
+
             validateShippingInformation: function () {
                 if (addressList().length === 0) {
                     var originalAddress = quote.shippingAddress();
