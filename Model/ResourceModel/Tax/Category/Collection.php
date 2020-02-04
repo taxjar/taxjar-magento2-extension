bb5df9b6040a4efed9458e20dd14d1020942cdcb
Ryan R <44789510+ryanreeves-taxjar@users.noreply.github.com>
2020-02-03 18:38:43 -0700

    PTC Expansion Refactor (#98)
    * Rework product tax category storage
    
    Storing product tax categories as a configuration value won't scale well
    as we continue to increase the total number of categories.  Reworking
    categories to be stored in their own table eliminates this problem and
    will support significantly more categories.
    
    * Add connect/disconnect actions, add table index
    
    Populate new categories from the API during setup.  Delete old
    categories when disconnecting from TaxJar.  Add an index to
    product_tax_code because it is used for lookups.  Misc code cleanup.
    
    * Sort the product tax categories array.  Bump the setup_version.

---
+++ Model/ResourceModel/Tax/Category/Collection.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Taxjar_SalesTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Taxjar
+ * @package    Taxjar_SalesTax
+ * @copyright  Copyright (c) 2019 TaxJar. TaxJar is a trademark of TPS Unlimited, Inc. (http://www.taxjar.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+namespace Taxjar\SalesTax\Model\ResourceModel\Tax\Category;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Taxjar\SalesTax\Model\Tax\Category', 'Taxjar\SalesTax\Model\ResourceModel\Tax\Category');
+    }
+}
