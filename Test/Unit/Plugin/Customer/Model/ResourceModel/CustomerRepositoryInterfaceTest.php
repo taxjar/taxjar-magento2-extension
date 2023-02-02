4e35d699345f75916525d8aa4eadb5e034f2ae27
Seth Obey <sethobey@stripe.com>
2023-02-02 16:18:32 -0600

    Injects logger into CustomerRepository plugin class to fix regression on failure

--- Test/Unit/Plugin/Customer/Model/ResourceModel/CustomerRepositoryInterfaceTest.php
+++ Test/Unit/Plugin/Customer/Model/ResourceModel/CustomerRepositoryInterfaceTest.php
@@ -3,9 +3,12 @@
 namespace Taxjar\SalesTax\Test\Unit\Plugin\Customer\Model\ResourceModel;
 
 use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Exception\LocalizedException;
 use PHPUnit\Framework\MockObject\MockObject;
 use Taxjar\SalesTax\Model\Client;
 use Taxjar\SalesTax\Model\ClientFactory;
+use Taxjar\SalesTax\Model\Configuration;
+use Taxjar\SalesTax\Model\Logger;
 use Taxjar\SalesTax\Plugin\Customer\Model\ResourceModel\CustomerRepository;
 use Taxjar\SalesTax\Test\Unit\UnitTestCase;
 
@@ -21,11 +24,20 @@ class CustomerRepositoryInterfaceTest extends UnitTestCase
      */
     private $clientFactoryMock;
 
+    /**
+     * @var MockObject|Logger
+     */
+    private $loggerMock;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->clientFactoryMock = $this->getMockBuilder(ClientFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->loggerMock = $this->getMockBuilder(Logger::class)
             ->disableOriginalConstructor()
             ->getMock();
     }
@@ -54,10 +66,37 @@ class CustomerRepositoryInterfaceTest extends UnitTestCase
         static::assertSame(123, $this->sut->beforeDeleteById($subjectMock, 123));
     }
 
+    public function testBeforeDeleteByIdFailureLogsError() {
+        $expectedMessage = 'Could not delete customer #123: Failed to initialize client';
+
+        $mockException = new LocalizedException(__('Failed to initialize client'));
+
+        $subjectMock = $this->getMockBuilder(CustomerRepositoryInterface::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $this->clientFactoryMock->expects(static::once())
+            ->method('create')
+            ->willThrowException($mockException);
+
+        $this->loggerMock->expects(static::once())
+            ->method('setFilename')
+            ->with(Configuration::TAXJAR_CUSTOMER_LOG);
+
+        $this->loggerMock->expects(static::once())
+            ->method('log')
+            ->with($expectedMessage);
+
+        $this->setExpectations();
+
+        static::assertSame(123, $this->sut->beforeDeleteById($subjectMock, 123));
+    }
+
     protected function setExpectations()
     {
         $this->sut = new CustomerRepository(
-            $this->clientFactoryMock
+            $this->clientFactoryMock,
+            $this->loggerMock
         );
     }
 }
